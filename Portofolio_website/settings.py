414364b3b817387c53daa3f926a77e7b31d8873f
Manta Sergiu Stefan <m.sergiu01@gmail.com>
2022-01-20 16:17:21 +0200

    .

--- Portofolio_website/settings.py
+++ Portofolio_website/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'sergiustefanmanta@gmail.com'
+EMAIL_HOST_PASSWORD = 'kennY2016!'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
